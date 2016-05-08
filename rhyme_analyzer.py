4563fd9e984c4dc9251f48d1e4b65f7054acb50c
Will Hennessy <hennessywill@gmail.com>
2016-05-08 17:35:16 -0500

    Score individual phoneme match for last three phonemes

--- rhyme_analyzer.py
+++ rhyme_analyzer.py
@@ -7,11 +7,13 @@ How to test in python console:
 
 from rhyme_analyzer import *
 import rhyme_analyzer
-sort_by_rhyme("I want a banana", ["I want an apple","I want a banana","I want a cherry","I want dominos"])
+sort_by_rhyme("Who cut other people open like cantaloupes", ["Who cut other people open like cantaloupes", "But if we can hump dead animals and antelopes","Then there's no reason that a man and another man can't elope","But if you feel like I feel, I got the antidote","Women wave your pantyhose, sing the chorus and it goes"])
 
 
 reload(rhyme_analyzer)  to update changes
 '''
+
+END_RHYME_LEN = 3
 
 THREE_CHAR_PHONEMES = ['aI@', 'aU@']
 TWO_CHAR_PHONEMES = ['l^','n^','3:','@L','@2','@5','aa','a#','A:','A@','e@','I2','i:','i@','u:','U@','O:','O@','o@','aI','eI','OI','aU','oU']
@@ -33,22 +35,58 @@ def rhyme_score(verse1, verse2):
     output: score of how well the verses rhyme together
     Returns a high score if the last word of the two verses are phonetically similar
     '''
-    phonemes1 = separate_phonemes(translate_to_phonemes(last_word(verse1)))
-    phonemes2 = separate_phonemes(translate_to_phonemes(last_word(verse2)))
+    phonemes1 = separate_phonemes(translate_to_phonemes(verse1))
+    phonemes2 = separate_phonemes(translate_to_phonemes(verse2))
 
     # TODO - define a better similarity function
     # consider removing consonants and focus only on vowels=rhymes
-    score = 1#SequenceMatcher(None, phonemes1, phonemes2).ratio()
+    # score = SequenceMatcher(None, phonemes1, phonemes2).ratio()
 
-    print verse1, '=>', phonemes1, verse2, '=>', phonemes2, 'Score:', score
+    score = 0
+    length = min(END_RHYME_LEN, len(phonemes1), len(phonemes2))
+    for back_idx in range(1, length+1):
+        score += phoneme_match(phonemes1[-back_idx], phonemes2[-back_idx])
+
+    print verse1, '=>', phonemes1
+    print verse2, '=>', phonemes2
+    print 'Score:', score
     print '----'
     return score
+
+
+def phoneme_match(one, two):
+    '''
+    input:  two phonemes
+    output: a score [0.0,1.0] of how well they rhyme
+
+    List of all phonemes:  http://espeak.sourceforge.net/phonemes.html
+    '''
+    uh = {'@','a#','@2', 'V'}
+    i = {'I','I2'}
+    ee = {'i','i:'}
+    orr = {'O@', 'o@'}
+    aa = {'a', 'aa'}
+    print one,two
+    if one == two:
+        return 1.0
+    elif one in uh and two in uh:
+        return 0.9
+    elif one in i and two in i:
+        return 0.9
+    elif one in orr and two in orr:
+        return 0.9
+    elif one in ee and two in ee:
+        return 0.7
+    elif one in aa and two in aa:
+        return 0.4
+    else:
+        return 0.0
 
 
 def translate_to_phonemes(verse):
     ''' NOTE: the shell=True parameter is a security hazard.
                 maybe shouldn't use it for web form input '''
-    command = u"espeak -xq -v%s -sep='[' %s" % ('en-us', verse)
+    command = u"espeak -xq -v%s '%s'" % ('en-us', verse)
     phonetics = subprocess.check_output(command, shell=True)
     return re.sub("[',\s+$]", '', phonetics)
 
